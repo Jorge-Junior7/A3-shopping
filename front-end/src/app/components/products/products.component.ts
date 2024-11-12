cd700d92db531d22237f63a4de3a1ba420e30648
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2024-11-11 22:02:54 -0300

    feat: implemetando search

--- front-end/src/app/components/products/products.component.ts
+++ front-end/src/app/components/products/products.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ProductsPreviewService } from '../../services/api/product_preview.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
@@ -15,6 +16,7 @@ export class ProductsComponent implements OnInit {
   paginatedProducts: any[] = [];
   currentPage = 1;
   productsPerPage = 20;
+  searchTerm = ''; // Variável para armazenar o termo de pesquisa
 
   constructor(private productsPreviewService: ProductsPreviewService) {}
 
@@ -28,10 +30,6 @@ export class ProductsComponent implements OnInit {
         this.products = data;
         this.updatePaginatedProducts();
         console.log('Produtos carregados:', this.products);
-        this.products.forEach((product) => {
-          console.log('Imagem do produto:', product.photo1);
-          console.log('Preço do produto:', product.price);
-        });
       },
       (error) => {
         console.error('Erro ao carregar produtos', error);
@@ -39,14 +37,29 @@ export class ProductsComponent implements OnInit {
     );
   }
 
+  // Método para atualizar os produtos paginados com base na pesquisa
   updatePaginatedProducts(): void {
+    const filteredProducts = this.getFilteredProducts();
     const startIndex = (this.currentPage - 1) * this.productsPerPage;
     const endIndex = startIndex + this.productsPerPage;
-    this.paginatedProducts = this.products.slice(startIndex, endIndex);
+    this.paginatedProducts = filteredProducts.slice(startIndex, endIndex);
+  }
+
+  // Método para retornar os produtos filtrados com base no termo de pesquisa
+  getFilteredProducts(): any[] {
+    return this.products.filter(product =>
+      product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  // Método para ativar a pesquisa e atualizar a lista paginada
+  searchProducts(): void {
+    this.currentPage = 1; // Redefine para a primeira página ao pesquisar
+    this.updatePaginatedProducts();
   }
 
   nextPage(): void {
-    if (this.currentPage * this.productsPerPage < this.products.length) {
+    if (this.currentPage * this.productsPerPage < this.getFilteredProducts().length) {
       this.currentPage++;
       this.updatePaginatedProducts();
     }
